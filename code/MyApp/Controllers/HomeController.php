ef3a7fd9e7f8db8d8cbfd197a2e4e876ce0d4e0d
freek <freek@infi.nl>
2014-06-10 21:55:20 +0200

    extra action test

--- code/MyApp/Controllers/HomeController.php
+++ code/MyApp/Controllers/HomeController.php
@@ -11,6 +11,11 @@ use PhpAspNetMvc\Types\Integer;
 use MyApp\Models\Customer;
 
 class HomeController extends ControllerBase{
+
+	public function homeAction() {
+		return $this->Content(new String("OK"));
+	}
+
 	public function indexAction(Customer $customer) {
 		return $this->Content(String::Format(
 			new String("id: {0}, name: {1}, street: {2}, zipcode: {3}"), 
